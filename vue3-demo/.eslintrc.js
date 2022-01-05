3e0c18aaf3eb2cb64674c943fc4d2b10a9eeac12
joo97 <13295923180@163.com>
2022-01-05 14:19:58 +0800

    1. add eslint config 2. add vue-transition-demo

---
+++ vue3-demo/.eslintrc.js
@@ -0,0 +1,50 @@
+/*
+ * @Description:
+ * @Autor: ZY
+ * @Date: 2020-12-07 10:30:20
+ * @LastEditors: ZY
+ * @LastEditTime: 2020-12-07 11:00:37
+ */
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': 'error',
+    'no-useless-escape': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/custom-event-name-casing': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
---
+++ vue3-demo/src/views/transition/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <button @click="show = !show">toggle</button>
+    <!-- <transition name="fade">
+      <div v-if="show">text</div>
+    </transition> -->
+    <transition name="bounce">
+      <div v-if="show">text</div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+
+.bounce-enter-active {
+  animation: bounce 0.6s;
+}
+.bounce-leave-active {
+  animation: bounce 0.5s reverse;
+}
+@keyframes bounce {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
